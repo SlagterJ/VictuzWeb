14bb42c68e00b08db554e019bdb9a685eb5b7719
Jordy Slagter <Jordy Slagter>
2024-09-26 10:07:04 +0200

    Add entity abstract model

---
+++ AbstractModels/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VictuzWeb.AbstractModels;
+
+/// <summary>
+/// Global base entity for the application, use this to extend every other model.
+/// </summary>
+public abstract class Entity
+{
+    /// <summary>
+    /// Globally unique identifier for this entity. Uses UUIDv4.
+    /// </summary>
+    [Key]
+    public Guid Identifier { get; set; }
+}
