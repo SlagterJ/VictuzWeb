a215bf68a23516a2e48beb21f49b1ce22e63c216
Jordy Slagter <Jordy Slagter>
2024-10-17 11:17:38 +0200

    add required modifier to properties

--- Models/User.cs
+++ Models/User.cs
@@ -10,30 +10,30 @@ public class User : Entity
     /// <summary>
     /// Firstname of the user.
     /// </summary>
-    public string Firstname { get; set; }
+    public required string Firstname { get; set; }
 
     /// <summary>
     /// Surname of the user.
     /// </summary>
-    public string Surname { get; set; }
+    public required string Surname { get; set; }
 
     /// <summary>
     /// Birthdate of the user.
     /// </summary>
-    public DateOnly BirthDate { get; set; }
+    public required DateOnly BirthDate { get; set; }
 
     /// <summary>
     /// Defines the role, and thus the permissions, of this user.
     /// </summary>
-    public Role Role { get; set; }
+    public required Role Role { get; set; }
 
     /// <summary>
     /// Suggestions suggested by this user.
     /// </summary>
-    public IEnumerable<Suggestion> Suggestions { get; set; }
+    public required IEnumerable<Suggestion> Suggestions { get; set; }
 
     /// <summary>
     /// The gatherings this user is registered for.
     /// </summary>
-    public IEnumerable<Gathering> RegisteredForGatherings { get; set; }
+    public required IEnumerable<Gathering> RegisteredForGatherings { get; set; }
 }
