b5a097c49de90f374ed2ec1f3680b2675a86eab1
Jordy Slagter <slagterj2001@gmail.com>
2024-10-29 10:10:55 +0100

    add club

---
+++ Models/Club.cs
@@ -0,0 +1,24 @@
+﻿using VictuzWeb.AbstractModels;
+
+namespace VictuzWeb.Models;
+
+/// <summary>
+/// A club.
+/// </summary>
+public class Club : Entity
+{
+    /// <summary>
+    /// Is this club accepted by an admin?
+    /// </summary>
+    public required bool Accepted { get; init; }
+
+    /// <summary>
+    /// The name of the club.
+    /// </summary>
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// The owner of this club.
+    /// </summary>
+    public required User Owner { get; init; }
+}
