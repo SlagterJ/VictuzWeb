1e1c21c7c9c59bd423c17a262e53ffb4158727fb
Jordy Slagter <slagterj2001@gmail.com>
2024-11-07 12:37:45 +0100

    you were expecting a commit description, but it was me, dio!

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const dioChance = Math.floor(Math.random() * 100) + 1;
+
+if (dioChance === 1) {
+    window.location = "https://cdn.discordapp.com/attachments/1283858486010187867/1304046012322549761/9k.png?ex=672df6fd&is=672ca57d&hm=6f954d7231be20fdea17dcf6be6348fc25bf92a7433e58c912868ade3dd7cf19&";
+}
