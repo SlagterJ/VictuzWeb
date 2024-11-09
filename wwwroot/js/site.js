73c8938e3be1974936ac87e72b05343f641fda03
Geert <geertbruls@outlook.com>
2024-11-09 23:07:56 +0100

    Fix Dio

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,5 +6,5 @@ const dioDisabled = false;
 const dioChance = Math.floor(Math.random() * 500) + 1;
 
 if (dioChance === 1 && dioDisabled === false) {
-    window.location = "https://cdn.discordapp.com/attachments/1283858486010187867/1304046012322549761/9k.png?ex=672df6fd&is=672ca57d&hm=6f954d7231be20fdea17dcf6be6348fc25bf92a7433e58c912868ade3dd7cf19&";
+    window.location = "https://i.ibb.co/2gCYNMg/dio.png";
 }
